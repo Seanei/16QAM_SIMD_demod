15863a5deba52c81c368af08620d10f6ef7926e5
Seanei <ivan9.6@mail.ru>
2023-12-19 22:54:04 +0300

    test added

--- demod.cpp
+++ demod.cpp
@@ -29,48 +29,68 @@ bool GetSvAndSign(const T& rx_symb, T& ref_sym, T& sv0, T& sv1, T& sv2, T& sv3){
     }
 }
 
-template <typename T, typename Y>
-void Demodulate(const T& rx_symb, Y& d1, Y& d2, Y& d3, Y& d4, double noise_var){
-    double real = rx_symb.real();
-    double imag = rx_symb.imag();
-    double real_abs = std::abs(real);
-    double imag_abs = std::abs(imag);
-    if (real_abs>= 2 * STEPFORQAM16 && imag_abs >= 2 * STEPFORQAM16){
-        auto d = - pow((real_abs - 3*STEPFORQAM16),2) - pow((imag_abs - 3*STEPFORQAM16),2);
-        d1 = (real >=0 ? 1 : -1) * (1/(noise_var))*( d + pow((imag_abs + 1*STEPFORQAM16),2) + pow((real_abs - 3*STEPFORQAM16),2)); 
-        d2 = (imag >=0 ? 1 : -1) * (1/(noise_var))*( d + pow((imag_abs - 3*STEPFORQAM16),2) + pow((real_abs + 1*STEPFORQAM16),2)); 
-        d3 = -1 * (1/(noise_var))*( d + pow((imag_abs - 1*STEPFORQAM16),2) + pow((real_abs - 3*STEPFORQAM16),2));  
-        d4 = -1 * (1/(noise_var))*( d + pow((imag_abs - 3*STEPFORQAM16),2) + pow((real_abs - 1*STEPFORQAM16),2));
-    } 
-    else if (real_abs <= 2 * STEPFORQAM16 && imag_abs >= 2 * STEPFORQAM16){
-        auto d = - pow((real_abs - 1*STEPFORQAM16),2) - pow((imag_abs - 3*STEPFORQAM16),2);
-        d1 = (real >=0 ? 1 : -1) * (1/(noise_var))*( d + pow((real_abs + 1*STEPFORQAM16),2) + pow((imag_abs - 3*STEPFORQAM16),2)); 
-        d2 = (imag >=0 ? 1 : -1) * (1/(noise_var))*( d + pow((real_abs - 1*STEPFORQAM16),2) + pow((imag_abs + 1*STEPFORQAM16),2)); 
-        d3 = 1 * (1/(noise_var))*( d + pow((real_abs - 3*STEPFORQAM16),2) + pow((imag_abs - 3*STEPFORQAM16),2));  
-        d4 = -1 * (1/(noise_var))*( d + pow((real_abs - 1*STEPFORQAM16),2) + pow((imag_abs - 1*STEPFORQAM16),2));
-    }
-    else if (real_abs >= 2 * STEPFORQAM16 && imag_abs <= 2 * STEPFORQAM16){
-        auto d = - pow((real_abs - 3*STEPFORQAM16),2) - pow((imag_abs - 1*STEPFORQAM16),2);
-        d1 = (real >=0 ? 1 : -1) * (1/(noise_var))*( d + pow((real_abs + 1*STEPFORQAM16),2) + pow((imag_abs - 1*STEPFORQAM16),2)); 
-        d2 = (imag >=0 ? 1 : -1) * (1/(noise_var))*( d + pow((real_abs - 3*STEPFORQAM16),2) + pow((imag_abs + 1*STEPFORQAM16),2)); 
-        d3 = -1 * (1/(noise_var))*( d + pow((real_abs - 1*STEPFORQAM16),2) + pow((imag_abs - 1*STEPFORQAM16),2));  
-        d4 = 1 * (1/(noise_var))*( d + pow((real_abs - 3*STEPFORQAM16),2) + pow((imag_abs - 3*STEPFORQAM16),2));
-    }else {
-        auto d = - pow((real_abs - 1*STEPFORQAM16),2) - pow((imag_abs - 1*STEPFORQAM16),2);
-        d1 = (real >=0 ? 1 : -1) * (1/(noise_var))*( d + pow((real_abs + 1*STEPFORQAM16),2) + pow((imag_abs - 1*STEPFORQAM16),2)); 
-        d2 = (imag >=0 ? 1 : -1) * (1/(noise_var))*( d + pow((real_abs - 1*STEPFORQAM16),2) + pow((imag_abs + 1*STEPFORQAM16),2)); 
-        d3 =  1 * (1/(noise_var)) * ( d + pow((real_abs - 3*STEPFORQAM16),2) + pow((imag_abs - 1*STEPFORQAM16),2));  
-        d4 = 1 * (1/(noise_var)) * ( d + pow((real_abs - 1*STEPFORQAM16),2) + pow((imag_abs - 3*STEPFORQAM16),2));
+template <typename T>
+void Demodulate(const std::vector<std::complex<T>>& rx_symbols, std::vector<T>& demodulated, double noise_var){
+    const uint BITS_PER_SYMB = 4;
+    T d1, d2, d3, d4;
+    for (uint i = 0; i < rx_symbols.size(); ++i){
+        T real = rx_symbols[i].real();
+        T imag = rx_symbols[i].imag();
+        T real_abs = std::abs(real);
+        T imag_abs = std::abs(imag);
+        if (real_abs>= 2 * STEPFORQAM16 && imag_abs >= 2 * STEPFORQAM16){
+            auto d = - pow((real_abs - 3*STEPFORQAM16),2) - pow((imag_abs - 3*STEPFORQAM16),2);
+            d1 = (real >=0 ? 1 : -1) * (1/(noise_var))*( d + pow((imag_abs + 1*STEPFORQAM16),2) + pow((real_abs - 3*STEPFORQAM16),2)); 
+            d2 = (imag >=0 ? 1 : -1) * (1/(noise_var))*( d + pow((imag_abs - 3*STEPFORQAM16),2) + pow((real_abs + 1*STEPFORQAM16),2)); 
+            d3 = -1 * (1/(noise_var))*( d + pow((imag_abs - 1*STEPFORQAM16),2) + pow((real_abs - 3*STEPFORQAM16),2));  
+            d4 = -1 * (1/(noise_var))*( d + pow((imag_abs - 3*STEPFORQAM16),2) + pow((real_abs - 1*STEPFORQAM16),2));
+        } 
+        else if (real_abs <= 2 * STEPFORQAM16 && imag_abs >= 2 * STEPFORQAM16){
+            auto d = - pow((real_abs - 1*STEPFORQAM16),2) - pow((imag_abs - 3*STEPFORQAM16),2);
+            d1 = (real >=0 ? 1 : -1) * (1/(noise_var))*( d + pow((real_abs + 1*STEPFORQAM16),2) + pow((imag_abs - 3*STEPFORQAM16),2)); 
+            d2 = (imag >=0 ? 1 : -1) * (1/(noise_var))*( d + pow((real_abs - 1*STEPFORQAM16),2) + pow((imag_abs + 1*STEPFORQAM16),2)); 
+            d3 = 1 * (1/(noise_var))*( d + pow((real_abs - 3*STEPFORQAM16),2) + pow((imag_abs - 3*STEPFORQAM16),2));  
+            d4 = -1 * (1/(noise_var))*( d + pow((real_abs - 1*STEPFORQAM16),2) + pow((imag_abs - 1*STEPFORQAM16),2));
+        }
+        else if (real_abs >= 2 * STEPFORQAM16 && imag_abs <= 2 * STEPFORQAM16){
+            auto d = - pow((real_abs - 3*STEPFORQAM16),2) - pow((imag_abs - 1*STEPFORQAM16),2);
+            d1 = (real >=0 ? 1 : -1) * (1/(noise_var))*( d + pow((real_abs + 1*STEPFORQAM16),2) + pow((imag_abs - 1*STEPFORQAM16),2)); 
+            d2 = (imag >=0 ? 1 : -1) * (1/(noise_var))*( d + pow((real_abs - 3*STEPFORQAM16),2) + pow((imag_abs + 1*STEPFORQAM16),2)); 
+            d3 = -1 * (1/(noise_var))*( d + pow((real_abs - 1*STEPFORQAM16),2) + pow((imag_abs - 1*STEPFORQAM16),2));  
+            d4 = 1 * (1/(noise_var))*( d + pow((real_abs - 3*STEPFORQAM16),2) + pow((imag_abs - 3*STEPFORQAM16),2));
+        }else {
+            auto d = - pow((real_abs - 1*STEPFORQAM16),2) - pow((imag_abs - 1*STEPFORQAM16),2);
+            d1 = (real >=0 ? 1 : -1) * (1/(noise_var))*( d + pow((real_abs + 1*STEPFORQAM16),2) + pow((imag_abs - 1*STEPFORQAM16),2)); 
+            d2 = (imag >=0 ? 1 : -1) * (1/(noise_var))*( d + pow((real_abs - 1*STEPFORQAM16),2) + pow((imag_abs + 1*STEPFORQAM16),2)); 
+            d3 =  1 * (1/(noise_var)) * ( d + pow((real_abs - 3*STEPFORQAM16),2) + pow((imag_abs - 1*STEPFORQAM16),2));  
+            d4 = 1 * (1/(noise_var)) * ( d + pow((real_abs - 1*STEPFORQAM16),2) + pow((imag_abs - 3*STEPFORQAM16),2));
+        }
+        demodulated[i*BITS_PER_SYMB] = std::move(d1);
+        demodulated[i*BITS_PER_SYMB+1] = std::move(d2);
+        demodulated[i*BITS_PER_SYMB+2] = std::move(d3);
+        demodulated[i*BITS_PER_SYMB+3] = std::move(d4);
+
     }
 }
 
-template <typename T, typename Y>
-void Demodulate_no_if(const T& rx_symb, Y& d1, Y& d2, Y& d3, Y& d4, double noise_var){
-    float dist_to_symb = std::pow(std::abs(std::abs(rx_symb.real()) - 2 * STEPFORQAM16) - STEPFORQAM16, 2) + std::pow(std::abs(std::abs(rx_symb.imag()) - 2 * STEPFORQAM16) - STEPFORQAM16, 2);
-    auto step_for_real = (static_cast<int>(std::abs(rx_symb.real())/(2*STEPFORQAM16)) * 2 + 1) * STEPFORQAM16;
-    auto step_for_imag = (static_cast<int>(std::abs(rx_symb.imag())/(2*STEPFORQAM16)) * 2 + 1) * STEPFORQAM16;
-    d1 = sign(rx_symb.real()) * (std::pow((std::abs(rx_symb.real()) + STEPFORQAM16), 2) + std:: pow(std::abs(rx_symb.imag()) - step_for_imag, 2) - dist_to_symb);
-    d2 = sign(rx_symb.imag()) * (std::pow((std::abs(rx_symb.real()) - step_for_real), 2) + std:: pow(std::abs(rx_symb.imag()) + STEPFORQAM16, 2) - dist_to_symb);
-    d3 = (-1) * sign(std::abs(rx_symb.real()) - 2 * STEPFORQAM16) * (std::pow(std::abs(std::abs(rx_symb.real()) - 2* STEPFORQAM16) + STEPFORQAM16, 2) + std::pow(std::abs(std::abs(rx_symb.imag()) - 2* STEPFORQAM16) - STEPFORQAM16, 2) - dist_to_symb);
-    d4 = (-1) * sign(std::abs(rx_symb.imag()) - 2 * STEPFORQAM16) * (std::pow(std::abs(std::abs(rx_symb.real()) - 2* STEPFORQAM16) - STEPFORQAM16, 2) + std::pow(std::abs(std::abs(rx_symb.imag()) - 2* STEPFORQAM16) + STEPFORQAM16, 2) - dist_to_symb);
+template <typename T>
+void Demodulate_no_if(const std::vector<std::complex<T>>& rx_symbols, std::vector<T>& demodulated, double noise_var){
+    const uint BITS_PER_SYMB = 4;
+    T re, im;
+    T d1, d2, d3, d4;
+    for (uint i = 0; i < rx_symbols.size(); ++i){
+        re = rx_symbols[i].real();
+        im = rx_symbols[i].imag();
+        float dist_to_symb = std::pow(std::abs(std::abs(re) - 2 * STEPFORQAM16) - STEPFORQAM16, 2) + std::pow(std::abs(std::abs(im) - 2 * STEPFORQAM16) - STEPFORQAM16, 2);
+        auto step_for_real = (static_cast<int>(std::abs(re)/(2*STEPFORQAM16)) * 2 + 1) * STEPFORQAM16;
+        auto step_for_imag = (static_cast<int>(std::abs(im)/(2*STEPFORQAM16)) * 2 + 1) * STEPFORQAM16;
+        d1 = sign(re) * (std::pow((std::abs(re) + STEPFORQAM16), 2) + std:: pow(std::abs(im) - step_for_imag, 2) - dist_to_symb);
+        d2 = sign(im) * (std::pow((std::abs(re) - step_for_real), 2) + std:: pow(std::abs(im) + STEPFORQAM16, 2) - dist_to_symb);
+        d3 = (-1) * sign(std::abs(re) - 2 * STEPFORQAM16) * (std::pow(std::abs(std::abs(re) - 2* STEPFORQAM16) + STEPFORQAM16, 2) + std::pow(std::abs(std::abs(im) - 2* STEPFORQAM16) - STEPFORQAM16, 2) - dist_to_symb);
+        d4 = (-1) * sign(std::abs(im) - 2 * STEPFORQAM16) * (std::pow(std::abs(std::abs(re) - 2* STEPFORQAM16) - STEPFORQAM16, 2) + std::pow(std::abs(std::abs(im) - 2* STEPFORQAM16) + STEPFORQAM16, 2) - dist_to_symb);
+        demodulated[i*BITS_PER_SYMB] = std::move(d1);
+        demodulated[i*BITS_PER_SYMB+1] = std::move(d2);
+        demodulated[i*BITS_PER_SYMB+2] = std::move(d3);
+        demodulated[i*BITS_PER_SYMB+3] = std::move(d4);
+    }
 }
--- main.cpp
+++ main.cpp
@@ -5,34 +5,84 @@
 #include <string>
 #include "demod.cpp"
 #include "demodSIMD.cpp"
+#include <cstdlib>
+#include <ctime>
+#include <chrono>
+
+const uint BITS_PER_SYMB = 4;
+const double RAND_MAX_DOUBLE = RAND_MAX;
+const int NUM_OF_SIMULATION = 100;
 
 int main(){
-    std::complex<double> num{0.9, 0.9};
-    double d1 = 0;
-    double d2 = 0;
-    double d3 = 0;
-    double d4 = 0;
-    double noise_var = 1;
-    std::vector<std::complex<double>> test2 = {{0.2, 0.9}, {-0.2,0.9}, {-0.2,-0.9}, {0.2,-0.9}};
-    std::vector<float> test1_for_simd_i(16, -0.2);
-    std::vector<float> test1_for_simd_q(16, 0.9);
-    //int N = 4;
-    std::vector<float> demodulated((test1_for_simd_i.size()), 0);
+    std::vector<int> ns_power = {6,7,8,9,10,11,12,13,14,15,16};
 
-    for(const auto& n : test2){
-        std::cout<<n<<std::endl;
-        Demodulate(n, d1, d2, d3, d4, noise_var);
-        std::cout << "Demodulated" << std::endl;
-        std::cout<<d1<<" "<<d2<<" "<<d3<<" "<<d4<<std::endl<<std::endl;
-        std::cout << "Demodulate_no_if" << std::endl;
-        Demodulate_no_if(n, d1, d2, d3, d4, noise_var);
-        std::cout<<d1<<" "<<d2<<" "<<d3<<" "<<d4<<std::endl<<std::endl;
+    for (auto p : ns_power) {
+        int NUMBER_OF_SYMBOLS = std::pow(2,p);
+        auto start = std::chrono::high_resolution_clock::now();
+        auto end = std::chrono::high_resolution_clock::now();
+        std::chrono::nanoseconds duration_demod, duration_demod_if, duration_demod_simd;
+
+
+        //std::chrono::duration duration_demod, duration_demod_if, duration_demod_simd;
+        for (int i = 0; i < NUM_OF_SIMULATION; ++i){
+
+            std::srand((unsigned)std::time(0));
+            double noise_var = 1;
+            std::vector<float> test_for_simd_i, test_for_simd_q;
+            test_for_simd_i.reserve(NUMBER_OF_SYMBOLS * BITS_PER_SYMB);
+            test_for_simd_q.reserve(NUMBER_OF_SYMBOLS * BITS_PER_SYMB);
+            std::vector<std::complex<float>> test;
+            test.reserve(NUMBER_OF_SYMBOLS * BITS_PER_SYMB);
+            std::vector<float> demodulated(NUMBER_OF_SYMBOLS * BITS_PER_SYMB, 0);
+            std::vector<float> demodulated_no_if(NUMBER_OF_SYMBOLS * BITS_PER_SYMB, 0);
+            std::vector<float> demodulated_SIMD(NUMBER_OF_SYMBOLS * BITS_PER_SYMB, 0);
+            
+            for(int n = 0; n < NUMBER_OF_SYMBOLS; ++n){
+                float I = (std::rand() - (RAND_MAX_DOUBLE/2))/(RAND_MAX_DOUBLE/2);
+                float Q = (std::rand() - (RAND_MAX_DOUBLE/2))/(RAND_MAX_DOUBLE/2);
+                for (int i = 0; i < BITS_PER_SYMB; ++i){
+                    test_for_simd_i.push_back(I);
+                    test_for_simd_q.push_back(Q);
+                }
+                test.push_back({I,Q});
+            }
+            {
+                auto start = std::chrono::high_resolution_clock::now();
+                Demodulate(test, demodulated, noise_var);
+                auto end = std::chrono::high_resolution_clock::now();
+                duration_demod += std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+            }
+            /*
+            {
+                auto start = std::chrono::high_resolution_clock::now();
+                Demodulate_no_if(test, demodulated_no_if, noise_var);
+                auto end = std::chrono::high_resolution_clock::now();
+                duration_demod_if += std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+            }
+            */
+            {
+                auto start = std::chrono::high_resolution_clock::now();
+                Demodulate16QAMSIMD(test_for_simd_i, test_for_simd_q, demodulated_SIMD);
+                auto end = std::chrono::high_resolution_clock::now();
+                duration_demod_simd += std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+            }
+        }
+        std::cout << "NUMBER_OF_SYMBOLS: 2^" << p <<std::endl;
+        std::cout << "Execution time: " << (duration_demod.count())/NUM_OF_SIMULATION << " microseconds" << std::endl;
+        std::cout << "Execution time: " << (duration_demod_if.count())/NUM_OF_SIMULATION << " microseconds" << std::endl;
+        std::cout << "Execution time: " << (duration_demod_simd.count())/NUM_OF_SIMULATION << " microseconds" << std::endl;
+        std::cout << "____________" << std::endl << std::endl;
+
     }
-    Demodulate16QAMSIMD(test1_for_simd_i, test1_for_simd_q, demodulated);
-    std::cout << "DEMOD len " << demodulated.size() << std::endl;
-    for (auto elem : demodulated){
-        std::cout << elem << " ";
+
+    /*
+    for (int i = 0; i < 4; ++i){
+        std::cout << "Demod: " << demodulated[i * BITS_PER_SYMB] << ", " << demodulated[i * BITS_PER_SYMB + 1] << ", " 
+        << demodulated[i * BITS_PER_SYMB + 2] << ", " << demodulated[i * BITS_PER_SYMB + 3] << std::endl;
+        std::cout << "Demod_no_if: " << demodulated_no_if[i * BITS_PER_SYMB] << ", " << demodulated_no_if[i * BITS_PER_SYMB + 1] << ", " 
+        << demodulated_no_if[i * BITS_PER_SYMB + 2] << ", " << demodulated_no_if[i * BITS_PER_SYMB + 3] << std::endl;
+        std::cout << "Demod_SIMD: " << demodulated_SIMD[i * BITS_PER_SYMB] << ", " << demodulated_SIMD[i * BITS_PER_SYMB + 1] << ", " 
+        << demodulated_SIMD[i * BITS_PER_SYMB + 2] << ", " << demodulated_SIMD[i * BITS_PER_SYMB + 3] << std::endl;
     }
-    
-    std::cout<<std::endl;
+    */
 }
